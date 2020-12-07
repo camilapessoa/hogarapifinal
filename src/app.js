ba537e72efa7047da6ed0ae8221e3a08414d1273
camilapessoa <camilabentespessoa@gmail.com>
2020-12-07 19:46:40 -0300

    add app.js

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const db = require("./configs/dbconnect")
+
+db.connect() //inserir caminho do bd - ativar o banco, conectar do repositorio
+
+app.use(cors())//
+app.use(express.json())
+
+
+
+//usar rotas
+
+const teste = require("./routes/teste") //testar servidor
+const solicitanteRoute = require("./routes/solicitanteRoutes")
+
+
+app.use("/", teste) //chama teste
+app.use("/solicitante", solicitanteRoute) //solicitante
+
+
+module.exports = app 
