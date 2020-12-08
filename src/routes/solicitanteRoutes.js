ba84b61db8593592eca58f475a9cfd4b184aa79e
camilapessoa <camilabentespessoa@gmail.com>
2020-12-08 07:23:11 -0300

    "add put in solicitantes routes"

--- src/routes/solicitanteRoutes.js
+++ src/routes/solicitanteRoutes.js
@@ -17,6 +17,9 @@ router.get('/nome/:nome', controller.getName)
 //http://localhost:33/solicitantes/criar
 router.post('/criar', controller.addSolicitante)
 
+//http://localhost:33/solicitantes/atualizar/[ID]"
+router.put('/atualizar', controller.updateSolicitante)
+
 //http://localhost:33/solicitantes/del?_id=5fceca15682d5931e43d97ce
 router.delete('/del', controller.deleteById)
 
