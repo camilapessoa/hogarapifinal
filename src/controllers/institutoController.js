93a7e63d2123eee6d7be2da81e30d11d18d377fd
camilapessoa <camilabentespessoa@gmail.com>
2020-12-08 07:12:28 -0300

    "add get by name & delete in instituto Controller"

--- src/controllers/institutoController.js
+++ src/controllers/institutoController.js
@@ -16,6 +16,25 @@ const getAllInst = (req, res) => {
   })
 }
 
+//GET getName - busca os institutos pelo nome - geral
+const getNameInst = (req, res) => {
+  solicitanteCollection.findOne({ nome: req.params.nome }, (error, nome) => {
+    if (nome) {
+      return res.status(200).json({
+        mensagem: "Apoiador encontrado",
+        nome
+
+      })
+
+    } else {
+      return res.status(500).send({
+        mensagem: "Apoiador não encontrado",
+        error
+      })
+    }
+  })
+}
+
 //Post - create para adicionar Instituto
 const addInstituto = (req,res) => {
   const institutoBody = req.body
@@ -33,7 +52,32 @@ const addInstituto = (req,res) => {
   })
 }
 
+//DELETE - deleta instituto por id específico e retorna mensagem - próprio solicitante e instituições com permissão" 
+const deleteInstById = (req, res) => {
+  const idParam = req.query
+  solicitanteCollection.findByIdAndDelete(idParam, (error, apoiador) => {
+    if (error) {
+      return res.status(500).send({
+        mensagem: "Algo inesperado aconteceu ao deletar",
+        error
+      })
+
+    } else {
+      if (apoiador) {
+        return res.status(200).send({
+          mensagem: "Cadastro apagado com sucesso"
+        })
+      } else {
+        return res.sendStatus(404)
+      }
+    }
+  })
+}
+
+
 module.exports = {
   getAllInst,
-  addInstituto
+  getNameInst,
+  addInstituto,
+  deleteInstById
 }
