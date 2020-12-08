6bec6055d25a84f034e77bd3d79a35218e262c99
camilapessoa <camilabentespessoa@gmail.com>
2020-12-08 05:49:18 -0300

    "update app.js bodyparser"

--- src/app.js
+++ src/app.js
@@ -2,27 +2,32 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const db = require("./configs/dbconnect")
+const bodyParser = require('body-parser')
 
 db.connect() //inserir caminho do bd - ativar o banco, conectar do repositorio
 
 app.use(cors())//
 app.use(express.json())
-
+app.use(bodyParser.json())
 
 
 //usar rotas
 
 const teste = require("./routes/teste") //testar servidor
+//chama solicitante
 const solicitanteRoute = require("./routes/solicitanteRoutes")
 
-//chama teste
+//chama instituto
+const institutoRoute = require("./routes/institutoRoutes")
+
+//teste
 app.use("/", teste) 
 
 //solicitante
 app.use("/solicitantes", solicitanteRoute) 
 
 //institutos
-
+app.use("/instituto", institutoRoute)
 
 
 
