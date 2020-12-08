907508a0818e7536b81752ea6f6515d94af0467f
camilapessoa <camilabentespessoa@gmail.com>
2020-12-08 04:19:24 -0300

    "inserindo modelo institutoSchema"

--- src/models/institutoSchema.js
+++ src/models/institutoSchema.js
@@ -1,4 +1,9 @@
+//chamar mongoose
+const mongoose = require('mongoose')
 
+//chamar Schema
+
+const Schema = mongoose.Schema
 
 /* _ID
 Razão Social / Nome
@@ -6,6 +11,53 @@ CNPJ
 Endereço
 Telefone
 Email
-Contato na empresa /
-  instituição / ONG
+Contato na empresa - Responsável direto/
+instituição / ONG
 Serviço prestado */
+
+const institutoSchema = new Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId, //tipo de dado de id
+    auto: true,
+    required: true
+  },
+  nomeRazao: {
+    type: String,
+    required: true
+  },
+
+  CNPJ: {
+    type: String,
+    required: true
+  },
+
+  endereco: {
+    type: String,
+    required: true
+  },
+
+  responsavelDireto: {
+    type: String,
+    required: true
+  },
+
+  Servico: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: false
+  },
+
+  imagem: {
+    type: String,
+    required: false
+
+  }
+})
+
+const institutoCollection = mongoose.model('instituto', institutoSchema)
+
+module.exports = institutoCollection;
